60aee8e29046f9aa347c5807b508e254ab56ee2a
Илья Кушнаренков <R3Mly@yandex.ru>
2023-01-10 00:48:11 +0400

    Add handlers

--- kittybot.py
+++ kittybot.py
@@ -1,6 +1,25 @@
-from telegram import Bot
+from telegram.ext import CommandHandler, Updater, Filters, MessageHandler
 
-bot = Bot(token='5862945651:AAGOB6kEbeXLHGV5OYVPwpRJJZ1yvMMrLEQ')
-chat_id = 395058176
-text = 'Здравствуй!'
-bot.send_message(chat_id, text)
+updater = Updater(token='5862945651:AAEQmlQ1yXGW7qRwfiOxcF3bkjh-is4vawk')
+
+def say_hi(update, context):
+    # Получаем информацию о чате, из которого пришло сообщение,
+    # и сохраняем в переменную chat
+    chat = update.effective_chat
+    context.bot.send_message(chat_id=chat.id, text='Здравстсвуй, я КотБот!')
+
+def wake_up(update, context):
+    chat = update.effective_chat
+    context.bot.send_message(chat_id=chat.id, text='Буду рад поработать для тебя.')
+
+updater.dispatcher.add_handler(CommandHandler('start', wake_up))
+# Регистрируется обработчик MessageHandler;
+# из всех полученных сообщений он будет выбирать только текстовые сообщения
+# и передавать их в функцию say_hi()
+updater.dispatcher.add_handler(MessageHandler(Filters.text, say_hi))
+
+# Метод start_polling() запускает процесс polling, 
+# приложение начнёт отправлять регулярные запросы для получения обновлений.
+updater.start_polling()
+# Бот будет работать до тех пор, пока не нажмете Ctrl-C
+updater.idle()
